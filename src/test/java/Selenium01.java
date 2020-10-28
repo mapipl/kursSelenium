e2b6d7cacffdcc758019e40bbd5acea1e0f544fa
Maciek P <maciek_p@interia.pl>
2020-10-28 21:10:28 +0100

    Selenium 1. Przygotowanie srodowiska programistycznego

---
+++ src/test/java/Selenium01.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Selenium01 {
+    @Test
+    public void demoTest() {
+        WebDriver driver = new ChromeDriver();
+    }
+}
