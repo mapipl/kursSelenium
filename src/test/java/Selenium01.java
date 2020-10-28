bd236fdf137025acde3304b72de00e5ecd8e634d
Maciek P <maciek_p@interia.pl>
2020-10-28 22:07:40 +0100

    Selenium 2. Inicjalizacja drivera i demo Selenium

--- src/test/java/Selenium01.java
+++ src/test/java/Selenium01.java
@@ -5,6 +5,8 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class Selenium01 {
     @Test
     public void demoTest() {
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
+        driver.navigate().to("https://google.pl");
     }
 }
