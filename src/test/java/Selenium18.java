1e39413aacee328ab65e923ca2b7fb66788eb793
Maciek P <maciek_p@interia.pl>
2020-10-31 22:16:20 +0100

    Selenium 18. Selektory CSS: atrybuty i laczenie warunkow

---
+++ src/test/java/Selenium18.java
@@ -0,0 +1,32 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.Point;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Selenium18 {
+    WebDriver driver;
+
+    @BeforeEach
+    public void driverSetup() {
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().window().setSize(new Dimension(1280, 720));
+        driver.manage().window().setPosition(new Point(8, 30));
+        driver.navigate().to("https://www.amazon.com/");
+    }
+
+    @AfterEach
+    public void driverQuit() {
+        driver.close();
+        driver.quit();
+    }
+
+    @Test
+    public void findElementsByCssSelector() {
+        driver.findElement(By.cssSelector("[data-template='layoutSwapToolBar'][role='navigation']"));
+    }
+}
