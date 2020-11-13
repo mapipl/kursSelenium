a8f3d912d070d4be121a4e7a67d77409879f7de9
Maciek P <maciek_p@interia.pl>
2020-11-13 14:12:41 +0100

    Selenium 50. Listy rozwijane

--- src/test/java/Selenium50.java
+++ src/test/java/Selenium50.java
@@ -1,15 +1,12 @@
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.List;
-import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 public class Selenium50 {
